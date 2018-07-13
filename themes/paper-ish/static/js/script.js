d756823d99698562777ffd40380fc2203b98fe5e
Ahmed Musallam <atyaf66@gmail.com>
2018-07-12 22:58:30 -0500

    format js/css

--- themes/paper-ish/static/js/script.js
+++ themes/paper-ish/static/js/script.js
@@ -7,12 +7,12 @@
     /**
      * execute function on load event. If load already happened, exec directly.
      */
-    onLoad: function (fn){
-      if(window.loaded){
+    onLoad: function (fn) {
+      if (window.loaded) {
         fn();
         return;
       }
-      if(!window.loaded){
+      if (!window.loaded) {
         window.addEventListener("load", function () {
           window.loaded = true;
           fn();
@@ -27,12 +27,12 @@
       var script = document.createElement('script');
       var prior = document.getElementsByTagName('script')[0];
       script.async = 1;
-      script.onload = script.onreadystatechange = function( _, isAbort ) {
-          if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
-              script.onload = script.onreadystatechange = null;
-              script = undefined;
-              if(!isAbort) { if(callback) callback(); }
-          }
+      script.onload = script.onreadystatechange = function (_, isAbort) {
+        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+          script.onload = script.onreadystatechange = null;
+          script = undefined;
+          if (!isAbort) { if (callback) callback(); }
+        }
       };
       script.src = source;
       prior.parentNode.insertBefore(script, prior);
@@ -43,10 +43,10 @@
      * credit: https://www.filamentgroup.com/lab/async-css.html 
      */
     getStyle: function (source) {
-      var myCSS = document.createElement( "link" );
+      var myCSS = document.createElement("link");
       myCSS.rel = "stylesheet";
       myCSS.href = source;
-      document.head.insertBefore( myCSS, document.head.childNodes[ document.head.childNodes.length - 1 ].nextSibling );
+      document.head.insertBefore(myCSS, document.head.childNodes[document.head.childNodes.length - 1].nextSibling);
     }
   };
 
@@ -58,7 +58,7 @@
     /*
      * Find all code elements and init code highlighting with highlightjs
      */
-    highlightCode: function (){
+    highlightCode: function () {
       var blocks = document.querySelectorAll('pre code');
       for (var i = 0; i < blocks.length; i++) {
         hljs.highlightBlock(blocks[i]);
@@ -68,9 +68,9 @@
     /* 
      * Downloads highlightjs and css and begins highlighting code 
      */
-    initHighlight: function (cb){
+    initHighlight: function (cb) {
       var blocks = document.querySelectorAll('pre code');
-      if (blocks.length && !window.hljs){
+      if (blocks.length && !window.hljs) {
         this.util.getScript('/js/vendor/highlight.min.js', this.highlightCode);
         this.util.getStyle('/css/vendor/highlight.default.min.css');
       }
@@ -82,7 +82,7 @@
      * credit: http://www.phpied.com/this-page-loaded-in-x-seconds/ and https://timkadlec.com/
      */
     loadTime: function () {
-      setTimeout(function(){
+      setTimeout(function () {
         window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
         var t = performance.timing || {};
         if (!t) return;
@@ -102,14 +102,14 @@
       }, false);
     },
 
-    init: function (){
+    init: function () {
       this.menuToggle();
       this.util.onLoad(this.loadTime);
       this.initHighlight();
     }
 
   };
-  
+
   /**
    *    MAIN
    */
